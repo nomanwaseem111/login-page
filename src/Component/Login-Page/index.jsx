c420e0aa69c0e016a40ded706181d7cba41a7aa4
nomanwaseem111 <nomanwaseem111@gmail.com>
2023-03-01 03:24:55 +0500

    login

---
+++ src/Component/Login-Page/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const index = () => {
+  return (
+    <div className="main">
+      <div className="child1"> 
+        <div className="form">
+        <h1>Welcome to a new user registration interface</h1>
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis
+          ex viverra mollis congue. Pellentesque ultrices egestas.
+        </h3>
+        </div>
+      </div>
+      <div className="child2">
+        <div >
+        <img  className="image" src="https://uploads-ssl.webflow.com/63ebad54b7031254e853ca32/63ebad736dfca82e6d5b2819_ZucksBotLogo.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
